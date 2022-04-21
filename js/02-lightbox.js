c9fa196e94cb5bf6b4232cdb7c8a3348b2872ff6
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-04-21 15:40:26 +0300

    delete comments

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,6 @@ const gallery = document.querySelector(".gallery");
 const markup = galleryItems.map(galleryImg).join(``);
 
 gallery.insertAdjacentHTML("beforeend", markup);
-// gallery.addEventListener(`click`, onShowImg);
 
 function galleryImg({ description, original, preview }) {
   return `
@@ -16,19 +15,6 @@ function galleryImg({ description, original, preview }) {
      </a>
 `;
 }
-
-// function onShowImg(event) {
-//   event.preventDefault();
-//   if (!event.target.tagName === `IMG`) {
-//     return;
-//   }
-//   const instance = SimpleLightbox.create(`
-//       <img src="${event.target.dataset.source}" width="800" height="600">
-//   `);
-
-//   instance.show();
-//   console.log(event.target.tagName === `IMG`);
-// }
 
 var lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
